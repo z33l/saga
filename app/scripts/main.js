625dc339a4d3ec8a4fb3960b20d14ada798707a1
z33l <z33l@users.noreply.github.com>
2014-11-21 15:32:30 +0000

    Load famous core

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,12 @@
 /* global famous */
+/* jshint unused:false */
+
+'use strict';
+
 console.log(famous);
 
+var Engine    = famous.core.Engine;
+var Context   = famous.core.Context;
+var Modifier  = famous.core.Modifier;
+var Surface   = famous.core.Surface;
 
