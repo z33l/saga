59c41b989394ae21d7d00af5c4465c9970479428
z33l <z33l@users.noreply.github.com>
2014-11-21 16:25:37 +0000

    Add ImageSurface

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,10 +5,11 @@
 
 console.log(famous);
 
-var Engine    = famous.core.Engine;
-var Context   = famous.core.Context;
-var Modifier  = famous.core.Modifier;
-var Surface   = famous.core.Surface;
+var Engine        = famous.core.Engine;
+var Context       = famous.core.Context;
+var Modifier      = famous.core.Modifier;
+var Surface       = famous.core.Surface;
+var ImageSurface  = famous.surfaces.ImageSurface;
 
 var context = Engine.createContext();
 
